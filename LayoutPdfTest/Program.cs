61e6c209fea46ca6377eafac1529ddae615399a1
Chris Anders <filmee24@gmail.com>
2025-04-12 15:13:50 +0200

    init

---
+++ LayoutPdfTest/Program.cs
@@ -0,0 +1,139 @@
+﻿using System.Diagnostics;
+using Marius.Yoga;
+using UglyToad.PdfPig.Content;
+using UglyToad.PdfPig.Core;
+using UglyToad.PdfPig.Fonts.Standard14Fonts;
+using UglyToad.PdfPig.Writer;
+
+namespace LayoutPdfTest;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var builder = new PdfDocumentBuilder();
+
+        var page = builder.AddPage(PageSize.A4, false);
+        var config = new YogaConfig();
+
+        var root = new YogaNode(config)
+        {
+            Width = page.PageSize.Width,
+            Height = page.PageSize.Height,
+            Margin = 10,
+            Padding = 10,
+        };
+
+        var header = new YogaNode(config)
+        {
+            Height = 100,
+            MarginTop = 15,
+            MarginLeft = 10,
+            MarginRight = 10,
+            FlexGrow = 0,
+            Data = "header"
+        };
+
+        var headerText = new YogaNode(config);
+        header.Add(headerText);
+
+        var footer = new YogaNode(config)
+        {
+            Height = 35,
+            MarginBottom = 5,
+            MarginLeft = 10,
+            MarginRight = 10,
+            FlexDirection = YogaFlexDirection.Row,
+            FlexGrow = 0,
+            Data = "footer"
+        };
+
+        var contentArea = new YogaNode(config)
+        {
+            FlexDirection = YogaFlexDirection.Row,
+            FlexGrow = 1,
+            MarginLeft = 10,
+            MarginRight = 10,
+            Data = "content"
+        };
+
+        var leftColumn = new YogaNode(config)
+        {
+            FlexGrow = 1,
+            MarginLeft = 10,
+            MarginRight = 10,
+            Data = "left"
+        };
+
+        var middleColumn = new YogaNode(config)
+        {
+            FlexGrow = 2,
+            MarginLeft = 10,
+            MarginRight = 10,
+            Data = "middle"
+        };
+
+        var rightColumn = new YogaNode(config)
+        {
+            FlexGrow = 1,
+            MarginLeft = 10,
+            MarginRight = 10,
+            Data = "right"
+        };
+
+        contentArea.Add(leftColumn);
+        contentArea.Add(middleColumn);
+        contentArea.Add(rightColumn);
+
+        root.StyleDirection = YogaDirection.LeftToRight;
+
+        root.Add(footer);
+        root.Add(contentArea);
+        root.Add(header);
+
+        root.CalculateLayout();
+
+        font = builder.AddStandard14Font(Standard14Font.Helvetica);
+
+        DrawBox(root, page);
+
+        var documentBytes = builder.Build();
+
+        File.WriteAllBytes("newPdf.pdf", documentBytes);
+        Process.Start(new ProcessStartInfo("newPdf.pdf") { UseShellExecute = true });
+    }
+
+    private static readonly (byte r,byte g,byte b)[] Colors =
+    {
+        (255, 0, 0),    // Rot
+        (0, 255, 0),    // Grün
+        (0, 0, 255),    // Blau
+        (255, 255, 0),  // Gelb
+        (255, 0, 255),  // Magenta
+        (0, 255, 255)   // Cyan
+    };
+
+    private static PdfDocumentBuilder.AddedFont font;
+    private static void DrawBox(YogaNode root, PdfPageBuilder page, int colorIndex = 0)
+    {
+        foreach (var child in root.Children)
+        {
+            var color = Colors[colorIndex % Colors.Length];
+            page.SetStrokeColor(color.r, color.g, color.b);
+            var boxPos = new PdfPoint(child.LayoutX, child.LayoutY);
+            page.DrawRectangle(boxPos, child.LayoutWidth, child.LayoutHeight, 1);
+
+            if (child.Data is string text)
+            {
+                page.AddText(text, 20, new PdfPoint(child.LayoutX + 5, child.LayoutY + 5), font);
+            }
+
+            DrawBox(child, page, colorIndex + 1);
+        }
+
+        PdfPoint ConvertToTopLeftOrigin(PdfPoint point)
+        {
+            return new PdfPoint(point.X, page.PageSize.Height - point.Y);
+        }
+    }
+}
---
+++ LayoutPdfTest/Yoga/YogaCollectFlexItemsRowValues.cs
@@ -0,0 +1,34 @@
+﻿/**
+ * Copyright (c) 2014-present, Facebook, Inc.
+ * Copyright (c) 2018-present, Marius Klimantavičius
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marius.Yoga
+{
+    public class YogaCollectFlexItemsRowValues
+    {
+        public int ItemsOnLine;
+        public double? SizeConsumedOnCurrentLine = 0;
+        public double? TotalFlexGrowFactors = 0;
+        public double? TotalFlexShrinkScaledFactors = 0;
+        public int EndOfLineIndex;
+        public List<YogaNode> RelativeChildren = new List<YogaNode>();
+        public double? RemainingFreeSpace = 0;
+        // The size of the mainDim for the row after considering size, padding, margin
+        // and border of flex items. This is used to calculate maxLineDim after going
+        // through all the rows to decide on the main axis size of owner.
+        public double? MainDimension = 0;
+        // The size of the crossDim for the row after considering size, padding,
+        // margin and border of flex items. Used for calculating containers crossSize.
+        public double? CrossDimension = 0;
+    }
+}
