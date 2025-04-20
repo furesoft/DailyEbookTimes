7ba1e655a46fbe5c26119bc5f5bfe5cb4f52a8d8
Chris Anders <filmee24@gmail.com>
2025-04-20 11:49:14 +0200

    add image autosize attribute

--- DailyEbookTimes/LayoutEngine/Nodes/ImageNode.cs
+++ DailyEbookTimes/LayoutEngine/Nodes/ImageNode.cs
@@ -6,6 +6,40 @@ namespace Moss.NET.Sdk.LayoutEngine.Nodes;
 public class ImageNode(YogaConfig config) : YogaNode(config)
 {
     public required string Src { get; set; }
+    public bool AutoSize { get; set; }
+
+    private byte[] Data;
+
+    public override void ReCalculate(PdfPageBuilder page)
+    {
+        base.ReCalculate(page);
+        Data = LoadImage();
+
+        if (AutoSize)
+        {
+            AdjustSize();
+        }
+    }
+
+    private void AdjustSize()
+    {
+        (int width, int height) dimension;
+        if (Src.EndsWith(".png"))
+        {
+            dimension = ReadPngDimension();
+        }
+        else if (Src.EndsWith(".jpg") || Src.EndsWith(".jpeg"))
+        {
+            dimension = ReadJpegDimension();
+        }
+        else
+        {
+            throw new ArgumentException($"invalid image format '{Src}'");
+        }
+
+        Width = dimension.width;
+        Height = dimension.height;
+    }
 
     public override void Draw(PdfPageBuilder page, double absoluteX, double absoluteY)
     {
@@ -16,7 +50,30 @@ public class ImageNode(YogaConfig config) : YogaNode(config)
 
         base.Draw(page, absoluteX, absoluteY);
 
-        byte[] imageBytes = null;
+        var boxPos = new PdfPoint(absoluteX, page.PageSize.Height - absoluteY - LayoutHeight);
+        var rect = new PdfRectangle(boxPos, new PdfPoint(boxPos.X + LayoutWidth, boxPos.Y + LayoutHeight));
+
+        PdfPageBuilder.AddedImage img;
+
+        if (Src.EndsWith(".png"))
+        {
+            img = page.AddPng(new MemoryStream(Data), rect);
+        }
+        else if (Src.EndsWith(".jpg") || Src.EndsWith(".jpeg"))
+        {
+            img = page.AddJpeg(new MemoryStream(Data), rect);
+        }
+        else
+        {
+            throw new ArgumentException($"invalid image format '{Src}'");
+        }
+
+        page.AddImage(img, rect);
+    }
+
+    private byte[] LoadImage()
+    {
+        byte[] imageBytes;
 
         if (Src.StartsWith("http") || Src.StartsWith("https"))
         {
@@ -28,32 +85,89 @@ public class ImageNode(YogaConfig config) : YogaNode(config)
             imageBytes = LayoutEngine.Layout.PathResolver.ReadBytes(Src);
         }
 
-        var boxPos = new PdfPoint(absoluteX, page.PageSize.Height - absoluteY - LayoutHeight);
-        var rect = new PdfRectangle(boxPos, new PdfPoint(boxPos.X + LayoutWidth, boxPos.Y + LayoutHeight));
-
-        PdfPageBuilder.AddedImage img = null;
-
-        if (Src.EndsWith(".png"))
-        {
-            img = page.AddPng(new MemoryStream(imageBytes), rect);
-        }
-        else if (Src.EndsWith(".jpg") || Src.EndsWith(".jpeg"))
-        {
-            img = page.AddJpeg(new MemoryStream(imageBytes), rect);
-        }
-        else
-        {
-            throw new ArgumentException($"invalid image format '{Src}'");
-        }
-
-        page.AddImage(img, rect);
+        return imageBytes;
     }
 
     internal override void SetAttribute(string name, string value)
     {
-        if (name == "src")
+        switch (name)
         {
-            Src = value;
+            case "src":
+                Src = value;
+                break;
+            case "autosize":
+                AutoSize = value == "true";
+                break;
         }
+    }
+
+    private (int width, int height) ReadPngDimension()
+    {
+        var stream = new MemoryStream(Data);
+
+        stream.Seek(16, SeekOrigin.Begin);
+        var buffer = new byte[4];
+        stream.ReadExactly(buffer, 0, 4);
+        var width = BitConverter.ToInt32(buffer.Reverse().ToArray(), 0);
+
+        stream.Seek(20, SeekOrigin.Begin);
+        stream.ReadExactly(buffer, 0, 4);
+        var height = BitConverter.ToInt32(buffer.Reverse().ToArray(), 0);
+
+        return (width, height);
+    }
+
+    private (int width, int height) ReadJpegDimension()
+    {
+        using var stream = new MemoryStream(Data);
+        var b1 = stream.ReadByte();
+        var b2 = stream.ReadByte();
+
+        if (b1 != 0xFF || b2 != 0xD8)
+            throw new InvalidOperationException("invalid JPEG.");
+
+        while (stream.Position < stream.Length)
+        {
+            int markerStart;
+            do
+            {
+                markerStart = stream.ReadByte();
+            }
+            while (markerStart != 0xFF && stream.Position < stream.Length);
+
+            int markerType;
+            do
+            {
+                markerType = stream.ReadByte();
+            }
+            while (markerType == 0xFF && stream.Position < stream.Length);
+
+            if (markerType >= 0xC0 && markerType <= 0xCF &&
+                markerType != 0xC4 && markerType != 0xC8 && markerType != 0xCC)
+            {
+                stream.ReadByte(); // High
+                stream.ReadByte(); // Low
+
+                stream.ReadByte(); // skip Precision
+
+                var height = (stream.ReadByte() << 8) + stream.ReadByte();
+                var width = (stream.ReadByte() << 8) + stream.ReadByte();
+
+                return (width, height);
+            }
+            else if (markerType is 0xD9 or 0xDA)
+            {
+                break;
+            }
+            else
+            {
+                var len = (stream.ReadByte() << 8) + stream.ReadByte();
+                if (len < 2)
+                    throw new InvalidOperationException("Invalid JPEG-Segment.");
+                stream.Seek(len - 2, SeekOrigin.Current);
+            }
+        }
+
+        throw new InvalidOperationException("SOF-Marker not found");
     }
 }
