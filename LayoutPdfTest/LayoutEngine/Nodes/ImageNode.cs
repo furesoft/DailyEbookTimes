a558a3016d9f6ed8809b65c0221afa22515c7628
Chris Anders <filmee24@gmail.com>
2025-04-20 07:39:36 +0200

    imgnode can now use jpeg images

--- LayoutPdfTest/LayoutEngine/Nodes/ImageNode.cs
+++ LayoutPdfTest/LayoutEngine/Nodes/ImageNode.cs
@@ -4,13 +4,9 @@ using UglyToad.PdfPig.Writer;
 
 namespace Moss.NET.Sdk.LayoutEngine.Nodes;
 
-public class ImageNode : YogaNode
+public class ImageNode(YogaConfig config) : YogaNode(config)
 {
     public required Uri Src { get; set; }
-
-    public ImageNode(YogaConfig config) : base(config)
-    {
-    }
 
     public override void Draw(PdfPageBuilder page, double absoluteX, double absoluteY)
     {
@@ -22,21 +18,37 @@ public class ImageNode : YogaNode
         base.Draw(page, absoluteX, absoluteY);
 
         byte[] imageBytes = null;
+        var url = Src.ToString();
         if (Src is { IsAbsoluteUri: true, Scheme: "http" or "https" })
         {
             //Todo: convert to moss rest template
             var template = new RestTemplate();
-            imageBytes = template.GetBytes(Src.ToString());
+            imageBytes = template.GetBytes(url);
         }
 
         if (Src.IsFile)
         {
-            imageBytes = File.ReadAllBytes(Src.ToString().Replace("file://", ""));
+            imageBytes = File.ReadAllBytes(url.Replace("file://", ""));
         }
 
         var boxPos = new PdfPoint(absoluteX, page.PageSize.Height - absoluteY - LayoutHeight);
         var rect = new PdfRectangle(boxPos, new PdfPoint(boxPos.X + LayoutWidth, boxPos.Y + LayoutHeight));
-        var img = page.AddPng(new MemoryStream(imageBytes), rect);
+
+        PdfPageBuilder.AddedImage img = null;
+
+        if (url.EndsWith(".png"))
+        {
+            img = page.AddPng(new MemoryStream(imageBytes), rect);
+        }
+        else if (url.EndsWith(".jpg") ||
+                 url.EndsWith(".jpeg"))
+        {
+            img = page.AddJpeg(new MemoryStream(imageBytes), rect);
+        }
+        else
+        {
+            throw new ArgumentException($"invalid image format '{url}'");
+        }
 
         page.AddImage(img, rect);
     }
