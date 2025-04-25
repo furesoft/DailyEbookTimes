9478e91ea34995c6ebb699ad96e7be0ca00690c6
Chris Anders <filmee24@gmail.com>
2025-04-25 18:07:46 +0200

    add naive text wrapping

--- DailyEbookTimes/LayoutEngine/Nodes/TextNode.cs
+++ DailyEbookTimes/LayoutEngine/Nodes/TextNode.cs
@@ -3,6 +3,12 @@ using UglyToad.PdfPig.Writer;
 
 namespace Moss.NET.Sdk.LayoutEngine.Nodes;
 
+public enum TextWrapping
+{
+    None,
+    Wrap
+
+}
 public class TextNode(YogaConfig config, Layout parentLayout) : YogaNode(config, parentLayout)
 {
     public double FontSize { get; set; } = 10;
@@ -10,6 +16,7 @@ public class TextNode(YogaConfig config, Layout parentLayout) : YogaNode(config,
     public string FontFamily { get; set; } = "Default";
 
     public Color? Color { get; set; } = Colors.Black;
+    public TextWrapping TextWrapping { get; set; } = TextWrapping.None;
 
     public TextDecoration TextDecoration { get; set; }
 
@@ -69,20 +76,53 @@ public class TextNode(YogaConfig config, Layout parentLayout) : YogaNode(config,
     public override void Draw(PdfPageBuilder page, double absoluteX, double absoluteY)
     {
         if (Display == YogaDisplay.None || string.IsNullOrEmpty(Text))
-        {
             return;
-        }
 
         base.Draw(page, absoluteX, absoluteY);
 
         var text = GetActualString();
         var font = ParentLayout.GetFont(FontFamily);
+        var maxWidth = LayoutWidth > 0 ? LayoutWidth : double.MaxValue;
 
-        var measuredText = page.MeasureText(text, FontSize, PdfPoint.Origin, font);
-        var maxAscent = measuredText.Max(g => g.GlyphRectangle.Top);
-        var minDescent = measuredText.Min(g => g.GlyphRectangle.Bottom);
-        var yPosition = page.PageSize.Height - absoluteY - maxAscent;
-        var textWidth = measuredText.Max(g => g.GlyphRectangle.Right) - measuredText.Min(g => g.GlyphRectangle.Left);
+        List<string> lines = new();
+
+        if (TextWrapping == TextWrapping.Wrap)
+        {
+            var hardLines = text.Split('\n');
+            foreach (var hardLine in hardLines)
+            {
+                var start = 0;
+                while (start < hardLine.Length)
+                {
+                    var length = 1;
+                    var line = "";
+                    while (start + length <= hardLine.Length)
+                    {
+                        var testLine = hardLine.Substring(start, length);
+                        var measured = page.MeasureText(testLine, FontSize, PdfPoint.Origin, font);
+                        var width = measured.Max(g => g.GlyphRectangle.Right) -
+                                    measured.Min(g => g.GlyphRectangle.Left);
+                        if (width > maxWidth)
+                            break;
+                        line = testLine;
+                        length++;
+                    }
+
+                    if (string.IsNullOrEmpty(line))
+                        line = hardLine[start].ToString();
+                    lines.Add(line);
+                    start += line.Length;
+                }
+            }
+        }
+        else
+        {
+            lines.AddRange(text.Split('\n'));
+        }
+
+        var measuredLine = page.MeasureText(lines[0], FontSize, PdfPoint.Origin, font);
+        var lineHeight = measuredLine.Max(g => g.GlyphRectangle.Top) - measuredLine.Min(g => g.GlyphRectangle.Bottom);
+        var y = page.PageSize.Height - absoluteY - measuredLine.Max(g => g.GlyphRectangle.Top);
 
         if (Color != null)
         {
@@ -90,18 +130,9 @@ public class TextNode(YogaConfig config, Layout parentLayout) : YogaNode(config,
             page.SetTextAndFillColor(Color.r, Color.g, Color.b);
         }
 
-        page.AddText(text, FontSize, new PdfPoint(absoluteX, yPosition), font);
-
-        var lineThickness = FontSize * 0.04;
-        if (TextDecoration == TextDecoration.Underline)
+        for (var i = 0; i < lines.Count; i++)
         {
-            var underlineY = yPosition + minDescent * 0.25;
-            page.DrawLine(new PdfPoint(absoluteX, underlineY), new PdfPoint(absoluteX + textWidth, underlineY), lineThickness);
-        }
-        else if (TextDecoration == TextDecoration.Strikethrough)
-        {
-            var strikeY = yPosition + (maxAscent * 0.4);
-            page.DrawLine(new PdfPoint(absoluteX, strikeY), new PdfPoint(absoluteX + textWidth, strikeY), lineThickness);
+            page.AddText(lines[i], FontSize, new PdfPoint(absoluteX, y - i * lineHeight), font);
         }
 
         page.ResetColor();
@@ -128,6 +159,9 @@ public class TextNode(YogaConfig config, Layout parentLayout) : YogaNode(config,
                 break;
             case "textdecoration":
                 TextDecoration = Enum.Parse<TextDecoration>(value, true);
+                break;
+            case "textwrapping":
+                TextWrapping = Enum.Parse<TextWrapping>(value, true);
                 break;
         }
     }
